7d712c44f2cb6262459c05c6442df89c6c65cc65
wangwu <wangwu228056@gmail.com>
2024-02-28 13:53:29 +0800

    新增员工业务代码开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 @Data
 public class EmployeeDTO implements Serializable {
 
+    private static final long serialVersionUID = 265603589514106109L;
     private Long id;
 
     private String username;
